3f9e326177420453b35f435c12cd131248b848f0
Patrakov Nikita <33065550+PatNik1998@users.noreply.github.com>
2018-12-21 13:34:13 +0300

    Update EmployeeService.java

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -2,11 +2,18 @@ package service;
 
 import model.Employee;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeService {
 
-    Employee getById(Integer id);
+    public List<Employee> getEmployees();
+
+    public Employee getEmployee(Integer id);
+
+    public void saveEmployee(Employee employee);
+
+    public void updateEmployee(Employee employee);
 
 
 
