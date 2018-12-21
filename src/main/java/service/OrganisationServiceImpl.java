7bf1accea98a5f0b9af2b7f9c11a5514eb6960c3
Patrakov Nikita <33065550+PatNik1998@users.noreply.github.com>
2018-12-21 13:39:34 +0300

    Update OrganisationServiceImpl.java

--- src/main/java/service/OrganisationServiceImpl.java
+++ src/main/java/service/OrganisationServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import repository.OfficeRepository;
 import repository.OrganisationRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class OrganisationServiceImpl implements OrganisationService  {
@@ -20,11 +21,31 @@ public class OrganisationServiceImpl implements OrganisationService  {
 
 
     @Override
-    public Organisation getById(Integer id) {
+    public Organisation getOrganisation(Integer id) {
         Optional<Organisation> organisation = repository.findById(id);
         if(organisation.isPresent()){
             return organisation.get();
         }
         return null;
+    }
+
+    @Override
+    public List<Organisation> getOrganisations() {
+
+        List<Organisation> organisations = repository.findAll();
+        return organisations;
+
+    }
+    @Override
+    public void saveOrganisation(Organisation organisation){
+
+        repository.save(organisation);
+
+    }
+    @Override
+    public void updateOrganisation(Organisation organisation) {
+
+        repository.save(organisation);
+
     }
 }
