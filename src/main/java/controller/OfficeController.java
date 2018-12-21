c398d3f5ffdd6c71152590627eaa46f266c935cf
Patrakov Nikita <33065550+PatNik1998@users.noreply.github.com>
2018-12-21 13:32:40 +0300

    Update OfficeController.java

--- src/main/java/controller/OfficeController.java
+++ src/main/java/controller/OfficeController.java
@@ -1,40 +1,59 @@
 package controller;
 
 
+import model.Employee;
 import model.Office;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.OfficeService;
 
+import java.util.List;
+
+
 @RestController
-@RequestMapping("api/office/{id")
+
 public class OfficeController {
     private final OfficeService service;
+
     @Autowired
-    public OfficeController(OfficeService service){
+    public OfficeController(OfficeService service) {
         this.service = service;
     }
 
-    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Office> getOffice(@PathVariable("id") Integer id ){
-        if(id == null){
-            return new ResponseEntity<Office>(HttpStatus.BAD_REQUEST);
-        }
+    @RequestMapping("api/office/{id}")
+    public Office getOffice(@PathVariable("id") Integer id) {
 
-        Office office = service.getById(id);
-
-        if(office == null){
-            return new ResponseEntity<Office>(HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<Office>(office, HttpStatus.OK);
+        return service.getOffice(id);
     }
+
+    @RequestMapping("api/office/list")
+    public List<Office> getOffices() {
+
+        List<Office> offices = service.getOffices();
+        return offices;
+    }
+
+    @RequestMapping("api/office/save")
+    public void saveOffice(Office office) {
+        service.saveOffice(office);
+        System.out.println("success");
+
+    }
+
+    @RequestMapping("api/office/update")
+    public void updateOffcie(@RequestBody Office office, @PathVariable(name = "id") Integer id) {
+
+        Office off = service.getOffice(id);
+
+        if (off != null) {
+
+            service.updateOffice(office);
+        }
+    }
+
 }
 
 
